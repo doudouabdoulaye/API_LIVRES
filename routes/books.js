5777342d5484023572e05b4295ecb2b65cb44bc1
doudouabdoulaye <abdoulayedoudou99@gmail.com>
2025-08-28 11:01:16 +0100

    api livres 2

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db"); // On récupère la connexion unique
+const { body, validationResult } = require("express-validator"); // Pour la validation des entrées
 
-// GET /books
+// GET /books //recupere de livres dans la base de données
 router.get("/", (req, res) => {
   db.all("SELECT * FROM books", [], (err, rows) => {
     if (err) return res.status(500).json({ error: err.message });
@@ -11,7 +12,13 @@ router.get("/", (req, res) => {
 });
 
 // POST /books
-router.post("/", (req, res) => {
+
+
+
+router.post("/",   [
+    body("title").notEmpty().withMessage("Le titre est requis"),
+    body("author").notEmpty().withMessage("L'auteur est requis"),
+  ], (req, res) => {
   const { title, author } = req.body;
   if (!title || !author) return res.status(400).json({ error: "Titre et auteur requis" });
 
